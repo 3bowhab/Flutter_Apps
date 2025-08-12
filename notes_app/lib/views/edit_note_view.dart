aeef25964c1962e8716a3309002ec8e449a027ab
3bowhab <ali.abdelwhab21@gmail.com>
2025-08-12 18:42:47 +0300

    finish ui of edit note view

--- notes_app/lib/views/edit_note_view.dart
+++ notes_app/lib/views/edit_note_view.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/custom_text_feild.dart';
 
 class EditNoteView extends StatelessWidget {
   const EditNoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        text: 'Edit Note',
-        icon: Icons.check,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: CustomAppBar(
+          text: 'Edit Note',
+          icon: Icons.check,
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 30),
+          children: [
+            CustomTextField(text: 'Title'),
+            const SizedBox(height: 20),
+            CustomTextField(text: 'Content', maxLines: 10),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
